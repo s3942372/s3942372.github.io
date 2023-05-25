10705568712fd7e321fbcccb643324ac49aa0ef9
s3942372 <112152260+s3942372@users.noreply.github.com>
2023-05-25 14:00:17 +1000

    Add files via upload

---
+++ cyberware/emergingdigitalculturesassignment3/cyberware/popup.js
@@ -0,0 +1,224 @@
+const popup = document.querySelector('.popup')
+const close = document.querySelector('.close')
+
+const popup1 = document.querySelector('.popup1')
+const close1 = document.querySelector('.close1')
+
+const popup2 = document.querySelector('.popup2')
+const close2 = document.querySelector('.close2')
+
+const popup3 = document.querySelector('.popup3')
+const close3 = document.querySelector('.close3')
+
+const popup4 = document.querySelector('.popup4')
+const close4 = document.querySelector('.close4')
+
+const popup5 = document.querySelector('.popup5')
+const close5 = document.querySelector('.close5')
+
+const popup6 = document.querySelector('.popup6')
+const close6 = document.querySelector('.close6')
+
+const popup7 = document.querySelector('.popup7')
+const close7 = document.querySelector('.close7')
+
+const popup8 = document.querySelector('.popup8')
+const close8 = document.querySelector('.close8')
+
+const popup9 = document.querySelector('.popup9')
+const close9 = document.querySelector('.close9')
+
+const popup10 = document.querySelector('.popup10')
+const close10 = document.querySelector('.close10')
+
+const popup11 = document.querySelector('.popup11')
+const close11 = document.querySelector('.close11')
+
+const popup12 = document.querySelector('.popup12')
+const close12 = document.querySelector('.close12')
+
+const popup13 = document.querySelector('.popup13')
+const close13 = document.querySelector('.close13')
+
+const popup14 = document.querySelector('.popup14')
+const close14 = document.querySelector('.close14')
+
+const popup15 = document.querySelector('.popup15')
+const close15 = document.querySelector('.close15')
+
+const popup16 = document.querySelector('.popup16')
+const close16 = document.querySelector('.close16')
+
+
+            window.onload = function(){
+                setTimeout(function(){
+                    popup.style.display="inline"
+                },10000)
+                setTimeout(function(){
+                    popup1.style.display="inline"
+                },12500)
+                setTimeout(function(){
+                    popup2.style.display="inline"
+                },13700)
+                setTimeout(function(){
+                    popup3.style.display="inline"
+                },14500)
+                setTimeout(function(){
+                    popup4.style.display="inline"
+                },15300)
+                setTimeout(function(){
+                    popup5.style.display="inline"
+                },15800)
+                setTimeout(function(){
+                    popup6.style.display="inline"
+                },16300)
+                setTimeout(function(){
+                    popup7.style.display="inline"
+                },16500)
+                setTimeout(function(){
+                    popup8.style.display="inline"
+                },16700)
+                setTimeout(function(){
+                    popup9.style.display="inline"
+                },16900)
+                setTimeout(function(){
+                    popup10.style.display="inline"
+                },17000)
+                setTimeout(function(){
+                    popup11.style.display="inline"
+                },17100)
+                setTimeout(function(){
+                    popup12.style.display="inline"
+                },17200)
+                setTimeout(function(){
+                    popup13.style.display="inline"
+                },17300)
+                setTimeout(function(){
+                    popup14.style.display="inline"
+                },17450)
+                setTimeout(function(){
+                    popup15.style.display="inline"
+                },17580)
+                setTimeout(function(){
+                    popup16.style.display="inline"
+                },17700)
+
+            }
+        
+            close.addEventListener('click', () => {
+                popup.style.display="none"
+            })
+
+            // popup2 //
+
+        
+            close1.addEventListener('click', () => {
+                popup1.style.display="none"
+            })
+
+             // popup3 //
+
+        
+             close2.addEventListener('click', () => {
+                popup2.style.display="none"
+            })
+
+            // popup4 //
+
+        
+            close3.addEventListener('click', () => {
+                popup3.style.display="none"
+            })
+
+            // popup5 //
+
+        
+            close4.addEventListener('click', () => {
+                popup4.style.display="none"
+            })
+
+              // popup6 //
+
+        
+              close5.addEventListener('click', () => {
+                popup5.style.display="none"
+            })
+
+              // popup7 //
+
+        
+              close6.addEventListener('click', () => {
+                popup6.style.display="none"
+            })
+
+              // popup8 //
+
+        
+              close7.addEventListener('click', () => {
+                popup7.style.display="none"
+            })
+
+
+             // popup9 //
+
+        
+             close8.addEventListener('click', () => {
+                popup8.style.display="none"
+            })
+
+
+             // popup10 //
+
+        
+             close9.addEventListener('click', () => {
+                popup9.style.display="none"
+            })
+
+             // popup11 //
+
+        
+             close10.addEventListener('click', () => {
+                popup10.style.display="none"
+            })
+
+             // popup12 //
+
+        
+             close11.addEventListener('click', () => {
+                popup11.style.display="none"
+            })
+
+             // popup13 //
+
+        
+             close12.addEventListener('click', () => {
+                popup12.style.display="none"
+            })
+
+             // popup14 //
+
+        
+             close13.addEventListener('click', () => {
+                popup13.style.display="none"
+            })
+
+             // popup15 //
+
+        
+             close14.addEventListener('click', () => {
+                popup14.style.display="none"
+            })
+
+             // popup16 //
+
+        
+             close15.addEventListener('click', () => {
+                popup15.style.display="none"
+            })
+
+            // popup17 //
+
+        
+            close16.addEventListener('click', () => {
+                popup16.style.display="none"
+            })
